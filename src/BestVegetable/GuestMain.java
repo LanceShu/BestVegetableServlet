b9f55a581f70dea4ee7b30317034f9a9ec8262d1
LanceShu <869924650@qq.com>
2017-12-11 23:27:02 +0800

    add the identification for user to logining

--- src/BestVegetable/GuestMain.java
+++ src/BestVegetable/GuestMain.java
@@ -1,8 +1,8 @@
 package BestVegetable;
 
 import Service.GuestUtil;
-import Service.ProductUtils;
-import sun.rmi.runtime.Log;
+import com.mchange.v1.util.CollectionUtils;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 
 @WebServlet(name = "guest",urlPatterns = "/guest")
 public class GuestMain extends HttpServlet{
@@ -20,7 +22,6 @@ public class GuestMain extends HttpServlet{
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
         request.setCharacterEncoding("UTF-8");
-
         PrintWriter pw = response.getWriter();
 
         /**
